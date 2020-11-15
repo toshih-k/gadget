7d1ddcb737b8ee619fcae7ff1a5f4c8018de329f
Toshihisa KATO <toshihk@gmail.com>
2020-11-15 15:35:53 +0900

    削除用のmutation作成

--- lib/gadget/mutation/delete.rb
+++ lib/gadget/mutation/delete.rb
@@ -7,6 +7,21 @@ module Gadget
           def delete_mutation_for(active_record_class)
             description "delete #{active_record_class.name}"
             argument active_record_class.primary_key, GraphQL::Types::ID, required: true
+
+            define_method("resolve") do |params|
+              params = params.as_json
+              instance = active_record_class.find(params[active_record_class.primary_key])
+              if instance.destroy
+                {
+                  Gadget::Common::Utility.result_field_name(active_record_class) => instance
+                }
+              else
+                {
+                  Gadget::Common::Utility.result_field_name(active_record_class) => instance,
+                  "errors" => instance.errors.full_messages
+                }
+              end
+            end
           end
         end
       end
--- test/dummy/test/graphql/mutation_test.rb
+++ test/dummy/test/graphql/mutation_test.rb
@@ -1,10 +1,19 @@
 require 'test_helper'
 
 class MutationTest < ActionDispatch::IntegrationTest
+  book_count = Book.count
   test "create mutation" do
     query = <<'GQL'
     mutation($input: CreateBookMutationInput!) {
       createBook(input: $input) {
+        book {
+          id
+          name
+          owner {
+            id
+            name
+          }
+        }
         clientMutationId
         errors
       }
@@ -30,9 +39,9 @@ GQL
     )
     assert_response 200
     res = JSON.parse(response.body)
-    p res
-    # assert_equal(book.name, res['data']['book']['name'])
-
+    book = Book.find(res['data']['createBook']['book']['id'])
+    assert_equal(Book.count, book_count + 1)
+    assert_equal(book.name, res['data']['createBook']['book']['name'])
   end
 
   test "update mutation" do
@@ -43,6 +52,14 @@ GQL
     query = <<'GQL'
     mutation($input: UpdateBookMutationInput!) {
       updateBook(input: $input) {
+        book {
+          id
+          name
+          owner {
+            id
+            name
+          }
+        }
         clientMutationId
         errors
       }
@@ -74,6 +91,43 @@ GQL
     assert_equal(book_count, Book.count)
     assert_equal(book_modified.name, modified_book_name)
     assert_equal(book_modified.owner.name, modified_owner_name)
+  end
+
+  test "delete mutation" do
+    book = Book.first
+    book_count = Book.count
+    query = <<'GQL'
+    mutation($input: DeleteBookMutationInput!) {
+      deleteBook(input: $input) {
+        book {
+          id
+          name
+          owner
+        }
+        clientMutationId
+        errors
+      }
+    }
+GQL
+    variables = {
+      input: {
+        id: book.id,
+      }
+    }
+    post(
+      '/graphql',
+      {
+        params: {
+          query: query,
+          variables: variables
+        },
+        as: :json
+      }
+    )
+    assert_response 200
+    res = JSON.parse(response.body)
+    book_modified = Book.find(book.id)
+    assert_equal(book_count, Book.count)
   end
 
 end
