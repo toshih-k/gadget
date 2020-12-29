90251c99828bf6f8ceada936e507615d03d3572c
Toshihisa KATO <toshihk@gmail.com>
2020-12-29 15:25:00 +0900

    change init

--- lib/gadget/railtie.rb
+++ lib/gadget/railtie.rb
@@ -1,31 +1,28 @@
 module Gadget
   class Railtie < ::Rails::Railtie
     initializer 'gadget' do |app|
-      ActiveSupport.on_load :action_view do
-        require 'gadget/common/utility'
-        require 'gadget/types/enum'
-        require 'gadget/types/object'
-        require 'gadget/types/input_object'
-        require 'gadget/query/index'
-        require 'gadget/query/show'
-        require 'gadget/mutation/create'
-        require 'gadget/mutation/update'
-        require 'gadget/mutation/delete'
+      require 'gadget/common/utility'
+      require 'gadget/types/enum'
+      require 'gadget/types/object'
+      require 'gadget/types/input_object'
+      require 'gadget/query/index'
+      require 'gadget/query/show'
+      require 'gadget/mutation/create'
+      require 'gadget/mutation/update'
+      require 'gadget/mutation/delete'
+      GraphQL::Schema::Object.include Gadget::Types::Object
+      GraphQL::Schema::InputObject.include Gadget::Types::InputObject
+      GraphQL::Schema::Enum.include Gadget::Types::Enum
 
-        GraphQL::Schema::Object.include Gadget::Types::Object
-        GraphQL::Schema::InputObject.include Gadget::Types::InputObject
-        GraphQL::Schema::Enum.include Gadget::Types::Enum
+      GraphQL::Schema::Object.include Gadget::Query::Index
+      GraphQL::Schema::Object.include Gadget::Query::Show
 
-        GraphQL::Schema::Object.include Gadget::Query::Index
-        GraphQL::Schema::Object.include Gadget::Query::Show
+      GraphQL::Schema::Object.include Gadget::Query::Index
+      GraphQL::Schema::Object.include Gadget::Query::Show
 
-        GraphQL::Schema::Object.include Gadget::Query::Index
-        GraphQL::Schema::Object.include Gadget::Query::Show
-
-        GraphQL::Schema::RelayClassicMutation.include Gadget::Mutation::Create
-        GraphQL::Schema::RelayClassicMutation.include Gadget::Mutation::Update
-        GraphQL::Schema::RelayClassicMutation.include Gadget::Mutation::Delete
-      end
+      GraphQL::Schema::RelayClassicMutation.include Gadget::Mutation::Create
+      GraphQL::Schema::RelayClassicMutation.include Gadget::Mutation::Update
+      GraphQL::Schema::RelayClassicMutation.include Gadget::Mutation::Delete
     end
   end
 end
--- lib/gadget/version.rb
+++ lib/gadget/version.rb
@@ -1,3 +1,3 @@
 module Gadget
-  VERSION = '0.1.4'
+  VERSION = '0.1.5'
 end
