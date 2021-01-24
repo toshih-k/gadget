76108b15ef2ca2527b4f7fcba0abec6b89fa90eb
Toshihisa KATO <toshihk@gmail.com>
2021-01-24 15:05:47 +0900

    ふぃｘ

--- test/dummy/db/migrate/20201110072322_create_books.rb
+++ test/dummy/db/migrate/20201110072322_create_books.rb
@@ -1,6 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
-    create_table :books do |t|
+    create_table books, comment: '本' do |t|
       t.string :name, comment: "name"
       t.references :owner, comment: "owner id"
       t.integer :inventory_type, limit: 1, comment: "inventory type:1:has stock,2:no stock,3:ebook"
