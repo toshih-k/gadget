5319f30f64ef3489a3c046684db7c5a5f48d98c8
Toshihisa KATO <toshihk@gmail.com>
2021-03-06 19:05:56 +0900

    fix

---
+++ test/tmp/app/graphql/mutations/create_shop_mutation.rb
@@ -0,0 +1,5 @@
+module Mutations
+  class CreateShopMutation < BaseMutation
+    create_mutation_for Shop
+  end
+end
--- test/tmp/app/graphql/types/shop_input_type.rb
+++
@@ -1,5 +0,0 @@
-module Types
-  class ShopInputType < Types::BaseInputObject
-    from_active_record Shop
-  end
-end
