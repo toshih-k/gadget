ca8ab1cc640c8ddc7d28464482454defb7d0c538
Toshihisa KATO <toshihk@gmail.com>
2021-03-09 20:36:10 +0900

    security update

--- test/tmp/app/graphql/types/shop_paginated_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  class ShopPaginatedType < Types::BaseObject
-    field :records, [Types::ShopType], null: false
-    field :total_count, GraphQL::Types::Int, null: false
-    field :current_page, GraphQL::Types::Int, null: false
-    field :per_page, GraphQL::Types::Int, null: false
-  end
-end
